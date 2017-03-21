23d2f7a6120a22d0f161b03ef0c6e2338fafb423
Piter98 <piotr.pal98@gmail.com>
2017-03-21 12:51:42 +0100

    py

---
+++ py/onp.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  onp.py
+#
+#  Copyright 2017 user <user@lap79>
+
+
+def main(args):
+    stos = []
+    onp = raw_input("Podaj wyrażenie: ")
+    operand =  ''
+    for znak in onp:
+        if znak != " " and znak.isdigit():
+            operand+=znak
+        elif znak == " " and len(operand):
+            stos.append(float(operand))
+            operand = ""
+        elif znak in ('*', '-', '/','+','%'):
+            a = str(stos.pop())
+            b = str(stos.pop())
+            stos.append(eval(b+znak+a))
+
+    print "Wynik: ", stos.pop()
+    #wykrywanie bledów
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
